fe2c15ac5f6bde6deeef4a8abfbf3ab5f477c645
Rati Montreewat <ratixoxo@gmail.com>
2024-07-03 14:55:34 +0700

    Build (./routes): Adding first privacy policy

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -1,5 +1,62 @@
-<script  lang="ts">
-    import Error from '$lib/ui/error/Error.svelte';
+<script lang="ts">
+    import {appName} from 'web-config';
+    // to do : addseo tags
+
 </script>
 
-<Error></Error>
+<main class="max-w-xl mx-auto">
+    <div class="p-5">
+
+        <a href="/" class="btn btn-ghost">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-5 h-5"
+            >
+                <path
+                 d="M15 10a.75.75 0 01-.75.75H7.612l2.158 1.96a.75.75 0 11-1.04 1.08l-3.5-3.25a.75.75 0 010-1.08l3.5-3.25a.75.75 0 111.04 1.08L7.612 9.25h6.638A.75.75 0 0115 10z"
+                />
+            </svg>
+            {" "}
+            Back
+        </a>
+
+        <h1 class="text-3xl font-extrabold pb-6">
+            Privacy Policy for {appName}
+        </h1>
+
+        <pre
+            class="leading-relaxed whitespace-pre-wrap"
+        >
+            {`Last Updated: 2024-07-01
+
+            Welcome to Redprint Wizard (https://redprint.ninja). This Privacy Policy explains how we collect, use, and protect your personal information when you use our website and services.
+
+            1. Information We Collect
+
+            Personal Data: We collect your name, email address, and payment information for order processing.
+            Non-Personal Data: We use web cookies to enhance your browsing experience.
+            2. Purpose of Data Collection
+
+            We collect your personal data to process your orders and provide you with our services.
+            3. Data Sharing
+
+            We do not share your personal data with any third parties.
+            4. Children's Privacy
+
+            We do not collect any data from children under the age of 13. If we become aware that we have inadvertently received personal information from a child under the age of 13, we will delete such information from our records.
+            5. Updates to the Privacy Policy
+
+            We may update this Privacy Policy from time to time. If we make any changes, we will notify you by email.
+            6. Contact Us
+
+            If you have any questions or concerns about this Privacy Policy, please contact us at rati.mont@gmail.com.
+            Thank you for choosing Redprint Wizard.
+
+            `}
+      </pre>
+
+    </div>
+
+</main>
