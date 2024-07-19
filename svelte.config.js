f7fbca6b43f33e468fb1e1e78036083d613d5497
Rati Montreewat <ratixoxo@gmail.com>
2024-07-19 10:28:05 +0700

    Build (/): Switching to generating a standalone Node server

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex'
 import { createHighlighter } from "@bitmachina/highlighter";
