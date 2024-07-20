3a7a80466240373319ff8c21e792cdf862c26023
Rati Montreewat <ratixoxo@gmail.com>
2024-07-20 20:01:33 +0700

    Feat (./src/routes/): Adding install code block in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import CopyBlock from '$lib/ui/components/CopyBlock.svelte';
 
-	// to do : add seo tags
 	import {appName} from 'web-config';
 </script>
 
 <section class="py-2 px-4 text-center">
+
 	<div class="max-w-auto md:max-w-lg mx-auto">
 
 		<p class="!text-2xl flex justify-center space-x-2 font-black my-8">
@@ -20,27 +20,46 @@
 			We are the team behind Redprint Wizard, an open-source code generator to develop OPStack ’s contracts
 		</p>
 
+		<div class="pt-3 pb-4">
+			<h1 class="m-4 font-semibold">Use it:</h1>
+			<CopyBlock
+				boxClass="p-2 rounded-box font-black text-primary max-w-xl mx-auto"
+				class="mb-5"
+				background="bg-primary-content"
+				copiedBackground="bg-success"
+				copiedColor="text-success-content"
+				text={`npm add -D redprint-wizard`}
+			/>
+			<p class="mt-6 text-base-300">
+				Find out more on
+				<a class="underline" href="https://github.com/Ratimon/redprint-wizard" target="_blank" rel="noreferrer"
+					>github</a
+				>
+			</p>
+		</div>
+
         <p class="!text-xl flex justify-center space-x-2 my-8">
             We are also building redprint-forge, a developer-friendly framework/library in solidity to modify & deploy OPStack ’s contracts in a modular style
 		</p>
+
+		<div class="pt-3 pb-4">
+			<h1 class="m-4 font-semibold">Use it:</h1>
+			<CopyBlock
+				boxClass="p-2 rounded-box font-black text-primary max-w-xl mx-auto"
+				class="mb-5"
+				background="bg-primary-content"
+				copiedBackground="bg-success"
+				copiedColor="text-success-content"
+				text={`npm add -D redprint-forge`}
+			/>
+			<p class="mt-6 text-base-300">
+				Find out more on
+				<a class="underline" href="https://github.com/Ratimon/redprint-forge" target="_blank" rel="noreferrer"
+					>github</a
+				>
+			</p>
+		</div>
        
 	</div>
 
-	<div class="pt-3 pb-4">
-		<h1 class="m-4 font-semibold">Use it:</h1>
-		<CopyBlock
-			boxClass="p-2 rounded-box font-black text-primary max-w-xl mx-auto"
-			class="mb-5"
-			background="bg-primary-content"
-			copiedBackground="bg-success"
-			copiedColor="text-success-content"
-			text={`npm add -D redprint-forge`}
-		/>
-		<p class="mt-6 text-base-300">
-			Find out more on
-			<a class="underline" href="https://github.com/Ratimon/redprint-forge" target="_blank" rel="noreferrer"
-				>github</a
-			>
-		</p>
-	</div>
 </section>
